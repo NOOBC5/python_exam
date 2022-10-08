7ac314ae68bf6a03d9d4aa5318e972d65c1923f8
NOOBC5 <109941800+NOOBC5@users.noreply.github.com>
2022-10-08 20:30:57 +0900

    Add files via upload

---
+++ for.py
@@ -0,0 +1,17 @@
+
+patterns = ['가위', '보', '보', '가위', '가위', '가위', '보', '가위', '바위', '보']
+for pattern in patterns:
+    """
+    pattern = pattern[0]
+    print(pattern)
+    pattern = pattern[1]
+    print(pattern)
+    pattern = pattern[2]
+    print(pattern)
+    pattern = pattern[3]
+    print(pattern)
+    pattern = pattern[4]
+    print(pattern)
+    ...
+    """
+    print(pattern)
