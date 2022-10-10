8030fc3f3ee366833b34f8fe2cfc0e2e55c9e04a
NOOBC5 <109941800+NOOBC5@users.noreply.github.com>
2022-10-10 15:33:43 +0900

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,27 @@
+# def return_false():
+    # print("함수return_false")
+    # return False
+    
+# def return_true():
+    # print("함수return_true")
+    # return_true
+    
+# print("테스트1")
+# a = return_false()
+# b = return_true()
+# if a and b:
+    # print(True)
+# else:
+    # print(False)
+    
+# print("테스트2")
+# if return_false() and return_true():
+    # print("True")
+# else:
+    # print(False)
+    
+dic = {"key2":"Value1"}
+if "key1" in dic and dic["key1"] == "Value1":
+    print("key1도 있고, 그 값은 Value11이네")
+else:
+    print("아니네")
---
+++ while.py
@@ -0,0 +1,5 @@
+selectde = None
+while selectde not in ['가위', '바위', '보']:
+    selectde = input('가위, 바위, 보 중에 선택하세요.')
+    
+print('선택된 값은: ',selectde)
